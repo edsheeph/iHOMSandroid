812b3bfb8a8a9408bb8f9001460752d009250e47
Edrian Callejo Apilan <apilanedrian@gmail.com>
2019-02-08 23:25:01 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/anggarisky/splashtohomeangga/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.anggarisky.splashtohomeangga;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button signIn;
+
+    ImageView bgapp, clover;
+    LinearLayout texthome, menus;
+    Animation frombottom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+
+
+        bgapp = (ImageView) findViewById(R.id.bgapp);
+        clover = (ImageView) findViewById(R.id.clover);
+//        textsplash = (LinearLayout) findViewById(R.id.textsplash);
+        texthome = (LinearLayout) findViewById(R.id.texthome);
+        menus = (LinearLayout) findViewById(R.id.menus);
+
+        bgapp.animate().translationY(-1500).setDuration(2000).setStartDelay(1000);
+        clover.animate().alpha(0).setDuration(2000).setStartDelay(1000);
+//        textsplash.animate().translationY(140).alpha(0).setDuration(800).setStartDelay(300);
+
+        texthome.startAnimation(frombottom);
+        menus.startAnimation(frombottom);
+
+        signIn = (Button) findViewById(R.id.signIn);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDashboard();
+            }
+        });
+
+
+    }
+
+    public void openDashboard() {
+        Intent intent = new Intent(this, dashboard.class);
+        startActivity(intent);
+
+    }
+}
